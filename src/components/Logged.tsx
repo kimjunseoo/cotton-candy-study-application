b2f8c9b03cdc54a6d142e7eeb1dc5c518282445e
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-02-01 21:52:31 +0900

    Logged 컴포넌트 디자인 퍼블리싱

--- src/components/Logged.tsx
+++ src/components/Logged.tsx
@@ -1,5 +1,29 @@
+import LobbyImg from "./LobbyImg";
+
+import {
+  LoginWrap,
+  LoginInfoWrapper,
+  LoginUserName,
+  LoginCreateJoinWrapper,
+  LoginCreateRoom,
+  LoginEnterRoomIn,
+  LoginSearchFeedBack,
+} from "../styles/components/Logged";
+
 const Logged = () => {
-  return <div>로그인 세션 인증 성공되었을때</div>;
+  return (
+    <LoginWrap>
+      <LobbyImg />
+      <LoginInfoWrapper>
+        <LoginUserName>솜사탕님, 환영합니다.</LoginUserName>
+        <LoginCreateJoinWrapper>
+          <LoginCreateRoom>새로운 스터디룸 생성하기</LoginCreateRoom>
+          <LoginEnterRoomIn>친구들의 스터디룸 입장하기 (초대코드)</LoginEnterRoomIn>
+          <LoginSearchFeedBack>피드백 검색하기</LoginSearchFeedBack>
+        </LoginCreateJoinWrapper>
+      </LoginInfoWrapper>
+    </LoginWrap>
+  );
 };
 
 export default Logged;
--- src/styles/components/Logged.tsx
+++ src/styles/components/Logged.tsx
@@ -1,1 +1,51 @@
 import styled from "@emotion/styled";
+
+export const LoginWrap = styled.div`
+  width: 100%;
+  height: 400px;
+`;
+
+export const LoginInfoWrapper = styled.div`
+  width: 100%;
+`;
+
+export const LoginUserName = styled.div`
+  margin: 5.3rem 0;
+  font-size: 23px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const LoginCreateJoinWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  font-size: 17px;
+`;
+
+export const LoginCreateRoom = styled.div`
+  padding: 14px 70px;
+  background-color: #ffd6eb;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+export const LoginEnterRoomIn = styled.div`
+  padding: 14px 26px;
+  background-color: #fce9aa;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+export const LoginSearchFeedBack = styled.div`
+  padding: 14px 102px;
+  background-color: #d6dbff;
+  border-radius: 10px;
+  cursor: pointer;
+  margin-bottom: 20px;
+`;
