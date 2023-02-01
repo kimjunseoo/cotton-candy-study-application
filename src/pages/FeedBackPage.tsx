1a39f938b529fee27e758654caef27f4700f335b
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-02-02 01:28:24 +0900

    FeedBackPage 퍼블리싱 (피드백 내용 wrapper쪽 스크롤 오류 이슈 현황 미해결

--- src/pages/FeedBackPage.tsx
+++ src/pages/FeedBackPage.tsx
@@ -1,5 +1,80 @@
+import {
+  FeedBackWrapper,
+  FeedBackTitle,
+  FeedBackGetUserWrap,
+  FeedBackInput,
+  FeedBackGetInfoBtn,
+  FeedBackSection,
+  FeedBackInfo,
+  FeedBackUserHeaderInfo,
+  FeedBackMsg,
+} from "../styles/pages/FeedBackPage";
+
 const FeedBackPage = () => {
-  return <div>여기 피드백 페이지임</div>;
+  return (
+    <FeedBackWrapper>
+      <FeedBackTitle>발표도우미</FeedBackTitle>
+      <FeedBackGetUserWrap>
+        <FeedBackInput type="text" placeholder="닉네임" />
+        <FeedBackGetInfoBtn>검색</FeedBackGetInfoBtn>
+      </FeedBackGetUserWrap>
+
+      <FeedBackSection>
+        <FeedBackInfo>
+          <FeedBackUserHeaderInfo>
+            <div>닉네임</div>
+            <div>방 #4885</div>
+          </FeedBackUserHeaderInfo>
+          <FeedBackMsg>
+            리액트는 최강 입니다. 프론트엔드 직업은 꿀맛같은 직업 입니다. 백엔드 직업은 주옥같은 직업 입니다. 여기는 피드백게시판 입니다. CRUD기능 구현은
+            너무너무 어렵습니다. javascript의 변화는 너무너무 빨라요.
+          </FeedBackMsg>
+        </FeedBackInfo>
+
+        <FeedBackInfo>
+          <FeedBackUserHeaderInfo>
+            <div>닉네임</div>
+            <div>방 #4885</div>
+          </FeedBackUserHeaderInfo>
+          <FeedBackMsg>
+            리액트는 최강 입니다. 프론트엔드 직업은 꿀맛같은 직업 입니다. 백엔드 직업은 주옥같은 직업 입니다. 여기는 피드백게시판 입니다. CRUD기능 구현은
+            너무너무 어렵습니다. javascript의 변화는 너무너무 빨라요.
+          </FeedBackMsg>
+        </FeedBackInfo>
+
+        <FeedBackInfo>
+          <FeedBackUserHeaderInfo>
+            <div>닉네임</div>
+            <div>방 #4885</div>
+          </FeedBackUserHeaderInfo>
+          <FeedBackMsg>
+            리액트는 최강 입니다. 프론트엔드 직업은 꿀맛같은 직업 입니다. 백엔드 직업은 주옥같은 직업 입니다. 여기는 피드백게시판 입니다. CRUD기능 구현은
+            너무너무 어렵습니다. javascript의 변화는 너무너무 빨라요.
+          </FeedBackMsg>
+        </FeedBackInfo>
+        <FeedBackInfo>
+          <FeedBackUserHeaderInfo>
+            <div>닉네임</div>
+            <div>방 #4885</div>
+          </FeedBackUserHeaderInfo>
+          <FeedBackMsg>
+            리액트는 최강 입니다. 프론트엔드 직업은 꿀맛같은 직업 입니다. 백엔드 직업은 주옥같은 직업 입니다. 여기는 피드백게시판 입니다. CRUD기능 구현은
+            너무너무 어렵습니다. javascript의 변화는 너무너무 빨라요.
+          </FeedBackMsg>
+        </FeedBackInfo>
+        <FeedBackInfo>
+          <FeedBackUserHeaderInfo>
+            <div>닉네임</div>
+            <div>방 #4885</div>
+          </FeedBackUserHeaderInfo>
+          <FeedBackMsg>
+            리액트는 최강 입니다. 프론트엔드 직업은 꿀맛같은 직업 입니다. 백엔드 직업은 주옥같은 직업 입니다. 여기는 피드백게시판 입니다. CRUD기능 구현은
+            너무너무 어렵습니다. javascript의 변화는 너무너무 빨라요.
+          </FeedBackMsg>
+        </FeedBackInfo>
+      </FeedBackSection>
+    </FeedBackWrapper>
+  );
 };
 
 export default FeedBackPage;
