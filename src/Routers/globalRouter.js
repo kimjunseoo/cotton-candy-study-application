769c39c52430ed607c3fd9e3c383bf0ebbb9bfaf
kimjunseoo <zz0905k@naver.com>
2023-02-22 12:36:03 +0900

    Fix: CORS Error
    API request 시, 발생할 수 있는 CORS 문제를 방지하도록 cors 패키지 추가 및 localhost 허용했습니다.

--- src/Routers/globalRouter.js
+++ src/Routers/globalRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 
 const __dirname = path.resolve();
 const globalRouter = express.Router();
 
-
+globalRouter.use("/", cors({ origin: "http://localhost:5173" }));
 globalRouter.route("*").get( (req, res) => {
     return res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
